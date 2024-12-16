b379a75ac3b5d513fa5d172b3beac6922318f90d
Infi <infiziert@protonmail.ch>
2024-12-16 22:43:36 +0100

    Fix small mistake

--- MOAction/Windows/Config/ConfigWindow.Settings.cs
+++ MOAction/Windows/Config/ConfigWindow.Settings.cs
@@ -167,7 +167,7 @@ public partial class ConfigWindow
                 }
             }
 
-            if (entry.Job >= 1)
+            if (entry.Job is > 0 and < uint.MaxValue)
             {
                 using var indent = ImRaii.PushIndent();
                 ExcelSheetSelector<Lumina.Excel.Sheets.Action>.ExcelSheetComboOptions actionOptions = new()
