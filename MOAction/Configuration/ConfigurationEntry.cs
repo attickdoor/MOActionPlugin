ed7ddb1839749ae3db8be204ebbb826f5adac0f8
Infi <infiziert@protonmail.ch>
2024-12-16 23:19:31 +0100

    - Switch from job string to uint in the config

--- MOAction/Configuration/ConfigurationEntry.cs
+++ MOAction/Configuration/ConfigurationEntry.cs
@@ -1,6 +1,7 @@
 ﻿using Dalamud.Game.ClientState.Keys;
 using System;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace MOAction.Configuration;
 
@@ -12,13 +13,27 @@ public class ConfigurationEntry
     public List<(string, uint)> Stack;
 
     public VirtualKey Modifier;
-    public string Job;
+    public uint JobIdx;
 
-    public ConfigurationEntry(uint baseId, List<(string, uint)> stack, VirtualKey modifier, string job)
+    public ConfigurationEntry(uint baseId, List<(string, uint)> stack, VirtualKey modifier, uint job)
     {
         BaseId = baseId;
         Stack = stack;
         Modifier = modifier;
-        Job = job;
+        JobIdx = job;
+    }
+
+    [JsonConstructor]
+    [Obsolete("This constructor is a one-time migration from the old job string to jobIdx uint. Added 16/12/2024")]
+    public ConfigurationEntry(uint baseId, List<(string, uint)> stack, VirtualKey modifier, uint jobIdx, string job = null)
+    {
+        BaseId = baseId;
+        Stack = stack;
+        Modifier = modifier;
+
+        if (job == null)
+            JobIdx = jobIdx;
+        else
+            JobIdx = uint.TryParse(job, out var num) ? num : 0;
     }
 }
